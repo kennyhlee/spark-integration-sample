e23b29cf1ec1a852be2b53d8bac434c0d64167cf
Kenny Lee <kennlee2@cisco.com>
2017-05-03 01:33:01 -0700

    bug fix

--- server.js
+++ server.js
@@ -224,6 +224,48 @@ function oauthFlowCompleted(state, access_token, refresh_token, res) {
         //var str = read(join(__dirname, '/togofurther/list-rooms.ejs'), 'utf8');
     });
 
+    // Retreive webhooks: GET https://api.ciscospark.com/v1/webhooks
+    var options_webhook = {
+        method: 'GET',
+        url: 'https://api.ciscospark.com/v1/webhooks,
+        headers:
+            {
+                "authorization": "Bearer " + access_token
+            }
+    };
+
+    request(options_webhook, function (error, response, body) {
+        if (error) {
+            debug("could not reach Cisco Spark to retreive webhook's details, error: " + error);
+            res.send("<p>Sorry, could not retreive your Cisco Spark webhook details. Try again...</p>");
+            return;
+        }
+
+        // Check the call is successful
+        if (response.statusCode != 200) {
+            debug("could not retreive your details, /webhooks returned: " + response.statusCode);
+            res.send("<p>Sorry, could not retreive your Cisco Spark webhook details. Try again...</p>");
+            return;
+        }
+
+        var json= JSON.parse(body);
+        if ((!json)) {
+            debug("could not parse webhook details: bad json payload");
+            res.send("<h1>OAuth Integration could not complete</h1><p>Sorry, could not retreive your Cisco Spark account details. Try again...</p>");
+            return;
+        }
+
+        debug("webhook details: " + json);
+
+        // Uncomment to send feedback via static HTML code
+        //res.send("<h1>OAuth Integration example for Cisco Spark (static HTML)</h1><p>So happy to meet, " + json.displayName + " !</p>");
+        // OR leverage an EJS template
+        var str = read(join(__dirname, '/www/display-name.ejs'), 'utf8');
+        var compiled = ejs.compile(str)({ "displayName": "kl" , "webhooks": json});
+        res.send(compiled);
+        //var str = read(join(__dirname, '/togofurther/list-rooms.ejs'), 'utf8');
+    });
+
 }
 
 
